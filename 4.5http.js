1c07bc9894e2802b55510f8c5fd8962297a7b6eb
Vitor Dilucca <vitordilucca@hotmail.com>
2023-03-19 00:07:28 -0300

    adicionado .gitignore p ignorar o nodemodules/ e trabalhando com npm

--- 4.5http.js
+++ 4.5http.js
@@ -1,11 +1,19 @@
 const http = require('http')
 
+//executa este codigo no terminal >node 4.5http.js
+//e recarrega a pagina no endereço localhost:5000 para ver a mensagem 
 const server = http.createServer((req,res)=>{
-  console.log(req)
-  //executa este codigo no terminal >node 4.5http.js
-  //e recarrega a pagina no endereço localhost:5000 para ver a mensagem 
-  res.write('Welcome to our home page')
-  res.end()
+  if (req.url === '/') {
+    res.end('Welcome to our home page')
+  }else if (req.url === '/about') {
+    res.end('Here is some history in our about page')
+  }else{
+    res.end(`
+    <h1>Oops! this page does not exist</h1>
+    <p>We canst seem to find the page you are looking for</p>
+    <a href="/">Home</a>
+    `)
+  }
 })
 
 server.listen(5000)
