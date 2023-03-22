78b9017c13440eeaa4eda4fd9179f06cc48a78f6
Vitor Dilucca <vitordilucca@hotmail.com>
2023-03-22 01:15:54 -0300

    dois jeitos de arrumar o problema dos promises ,promise syntax ou async await

--- JS Nuggets/17AsyncAwait.js
+++ JS Nuggets/17AsyncAwait.js
@@ -10,6 +10,9 @@ const articles = [
   { userId: 3, articles: ['six', 'seven', 'eight', 'nine'] },
 ]
 
+
+
+
 function getUser(name) {
   return new Promise((resolve, reject) => {
     const user = users.find((user) => user.name === name)
@@ -23,12 +26,33 @@ function getUser(name) {
 
 function getArticles(userId) {
   return new Promise((resolve, reject) => {
-    const userArticles = articles.find((user)=>user.userId===userId)
+    const userArticles = articles.find((user) => user.userId === userId)
 
-    if(userArticles){
+    if (userArticles) {
       return resolve(userArticles.articles)
-    }else{
+    } else {
       reject(`Wrong ID`)
     }
   })
 }
+
+//jeito promises menos efetivo
+//retorna os artigos da pessoa escolhida
+
+/* getUser('anna')
+  .then((user) => getArticles(user.id))
+  .then((articles) => console.log(articles))
+  .catch((err) => console.log(err)) */
+
+//jeito async/await mais efetivo
+//retorna os artigos da pessoa escolhida
+async function getData() {
+  try {
+    const user = await getUser('john')
+    const articles = await getArticles(user.id)
+    console.log(articles)
+  } catch (error) {
+    console.log(error)
+  }
+}
+getData()
