4cd3c0a6d9d29e4bae0c75bb5496f0198e48805f
Vitor Dilucca <vitordilucca@hotmail.com>
2023-03-26 03:37:33 -0300

    jeito mais legal usando util.promisify para simplificar

--- 6.1asyncIMPORTANT.js
+++ 6.1asyncIMPORTANT.js
@@ -3,30 +3,33 @@ const fs = require('fs')
 const util = require('util')
 
 //promise do jeito bem feito usando a lib util(sem terminar)
-// const readFilePromise = util.promisify(readFile)
-// const writeFilePromise = util.promisify(writeFile)
+const readFilePromise = util.promisify(fs.readFile)
+const writeFilePromise = util.promisify(fs.writeFile)
 
-//Promise do jeito ruim
-function getText(path){
-  return new Promise((resolve, reject) => {
-    fs.readFile(path, 'utf-8', (err, result) => {
-      if (err) {
-        reject(err)
-      } else {
-        resolve(result)
-      }
-    })
-  })
-}
 
 //com async-await
 const start = async()=>{
   try {
-    const first = await getText('./4.2content/first.txt')
-    const second = await getText('./4.2content/second.txt')
+    const first = await readFilePromise('./4.2content/first.txt', 'utf8')
+    const second = await readFilePromise('./4.2content/second.txt', 'utf8')
+    await writeFilePromise(
+      './4.2content/result-6.1async.txt',`THIS IS AWESOMENESS: ${first} ======= ${second}`)
     console.log(first,second)
   } catch (error) {
     console.log(error)
   }
 }
 start()
+
+    //Promise do jeito ruim
+    // function getText(path){
+    //   return new Promise((resolve, reject) => {
+    //     fs.readFile(path, 'utf-8', (err, result) => {
+    //       if (err) {
+    //         reject(err)
+    //       } else {
+    //         resolve(result)
+    //       }
+    //     })
+    //   })
+    // }
