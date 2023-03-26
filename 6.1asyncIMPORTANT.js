d90b564548eb8ae1c3cd574e26c2a0a34115cbc1
Vitor Dilucca <vitordilucca@hotmail.com>
2023-03-26 16:33:37 -0300

    jeito ainda melhor sem util promisify simplesmente na importação do fs adiciona um .promises no final super simples

--- 6.1asyncIMPORTANT.js
+++ 6.1asyncIMPORTANT.js
@@ -1,19 +1,21 @@
 //codigo usando promises ainda
-const fs = require('fs')
-const util = require('util')
+const fs = require('fs').promises
+// const util = require('util')
 
-//promise do jeito bem feito usando a lib util(sem terminar)
-const readFilePromise = util.promisify(fs.readFile)
-const writeFilePromise = util.promisify(fs.writeFile)
+// //promise do jeito bem feito usando a lib util(sem terminar)
+// const readFilePromise = util.promisify(fs.readFile)
+// const writeFilePromise = util.promisify(fs.writeFile)
 
 
 //com async-await
 const start = async()=>{
   try {
-    const first = await readFilePromise('./4.2content/first.txt', 'utf8')
-    const second = await readFilePromise('./4.2content/second.txt', 'utf8')
-    await writeFilePromise(
-      './4.2content/result-6.1async.txt',`THIS IS AWESOMENESS: ${first} ======= ${second}`)
+    const first = await fs.readFile('./4.2content/first.txt', 'utf8')
+    const second = await fs.readFile('./4.2content/second.txt', 'utf8')
+    await fs.writeFile(
+      './4.2content/result-6.1async.txt',
+      `THIS IS AWESOMENESS: ${first} ======= ${second}`,
+      {flag: 'a'})
     console.log(first,second)
   } catch (error) {
     console.log(error)
