8c5c752a3a6b503ba2f9787107f2d31032ba7b9d
Vitor Dilucca <vitordilucca@hotmail.com>
2023-03-27 13:04:28 -0300

    creating web server local

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,29 @@
-console.log('Express Tutorial')
+const http = require('http')
+const {readFileSync, read} = require('fs')
+
+//get all files
+const homePage = readFileSync('./index.html')
+
+const server = http.createServer((req, res) => {
+  // console.log(req.method)
+  const url = req.url
+  if(url==='/'){
+    // res.writeHead(200, { 'content-type': 'text/plain' })
+    res.writeHead(200, { 'content-type': 'text/html' })
+    // res.write('<h1>home page</h1>')
+    res.write(homePage)
+    res.end()
+  }else if(url==='/about'){
+    res.writeHead(200, { 'content-type': 'text/html' })
+    res.write('<h1>ABOUT page</h1>')
+    res.end()
+    
+  }else{
+    res.writeHead(404, { 'content-type': 'text/html' })
+    res.write('<h1>PAGE NOT FOUND</h1>')
+    res.end()
+    
+  }
+})
+
+server.listen(5000)
