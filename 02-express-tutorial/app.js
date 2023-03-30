bd3a086528e3d2f9e3bcab62c08c3238db60c6c5
Vitor Dilucca <vitordilucca@hotmail.com>
2023-03-30 05:48:11 -0300

    http verb methods get,post,put,delete

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,35 +1,76 @@
-const app = require('express')()
-const authorize = require('./authorize')
+const express = require('express')
+const app = express()
+let { people } = require('./data')
 
-app.use(
-  [
-    function logger(req, res, next) {
-      const method = req.method
-      const url = req.url
-      const time = `${new Date().getFullYear()}`
-      console.log(method, url, time);
-      // res.send('Test')
-      next()
-    },
+//static access
+app.use(express.static('./11methods-public/'))
+//parse form data
+app.use(express.urlencoded({ extended: false }))
+//parse json
+app.use(express.json())
 
-    authorize
-  ]
-)
-
-//req => middleware => res
-app.get('/', (req, res) => {
-  res.send('Home')
+app.post('/login', (req, res) => {
+  const { name } = req.body
+  if (name) {
+    return res.status(200).send(`Welcome ${name}`)
+  }
+  res.status(401).send('Please Provide Credentials ')
 })
 
-app.get('/about', (req, res) => {
-  res.send('About')
+app.get('/api/people', (req, res) => {
+  res.status(200).json({ success: true, data: people })
 })
 
-app.get('/api/items', (req, res) => {
-  console.log(req.user);
-  res.send('Items')
+app.post('/api/people', (req, res) => {
+  const { name } = req.body
+  if (!name) {
+    return res
+      .status(400)
+      .json({ success: false, msg: 'Please provide name' })
+  }
+  res.status(201).json({ success: true, person: name })
+})
+
+app.post('/api/postman/people', (req, res) => {
+  const { name } = req.body
+  if (!name) {
+    return res
+      .status(400)
+      .json({ success: false, msg: 'Please provide name' })
+  }
+  res.status(201).json({ success: true, data: [...people, name] })
+})
+
+app.put('/api/people/:id', (req, res) => {
+  const { id } = req.params
+  const { name } = req.body
+  const person = people.find((person) => person.id === Number(id))
+
+  if (!person) {
+    return res
+      .status(404)
+      .json({ success: false, msg: `no person with id ${id}` })
+  }
+  const newPeople = people.map((person) => {
+    if (person.id === Number(id)) {
+      person.name = name
+    }
+    return person
+  })
+  res.status(200).json({ success: true, data: newPeople })
+})
+
+app.delete('/api/people/:id', (req, res) => {
+  const person = people.find((person) => person.id === Number(req.params.id))
+  if (!person) {
+    return res
+      .status(404)
+      .json({ success: false, msg: `no person with id ${req.params.id}` })
+  }
+  const newPeople = people.filter((person) => person.id !== Number(req.params.id))
+  return res.status(200).json({success:true, data:newPeople})
 })
 
 app.listen(5000, () => {
-  console.log('Server is listening on port 5000...')
-}) 
+  console.log("Server is listening on port 5000")
+})
--- 02-express-tutorial/final/10-middleware-options.js
+++ 02-express-tutorial/final/10-middleware-options.js
@@ -1,28 +1,35 @@
-const express = require('express')
-const app = express()
+const app = require('express')()
+// const authorize = require('./authorize')
 const morgan = require('morgan')
-const logger = require('./logger')
-const authorize = require('./authorize')
-//  req => middleware => res
+//req => middleware => res
+/* app.use(
+  [
+    function logger(req, res, next) {
+      const method = req.method
+      const url = req.url
+      const time = `${new Date().getFullYear()}`
+      console.log(method, url, time);
+      // res.send('Test')
+      next()
+    },
 
-// app.use([logger, authorize])
-// app.use(express.static('./public'))
+    authorize
+  ]
+) */
 app.use(morgan('tiny'))
-
 app.get('/', (req, res) => {
   res.send('Home')
 })
+
 app.get('/about', (req, res) => {
   res.send('About')
 })
-app.get('/api/products', (req, res) => {
-  res.send('Products')
-})
+
 app.get('/api/items', (req, res) => {
-  console.log(req.user)
+  console.log(req.user);
   res.send('Items')
 })
 
 app.listen(5000, () => {
-  console.log('Server is listening on port 5000....')
-})
+  console.log('Server is listening on port 5000...')
+}) 
