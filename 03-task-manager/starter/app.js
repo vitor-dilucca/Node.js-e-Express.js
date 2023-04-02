53035a85ab1ce375e537fca8d1b9af40ad064f73
Vitor Dilucca <vitordilucca@hotmail.com>
2023-04-02 04:48:44 -0300

    fazendo conexao com o mongodb criado pasta db com o connect.js o server ja foi criado no mongodb atlas, e tem um par de dados de exemplo

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const app = express()
+const tasks=require('./routes/tasks')
 const port = 3000
 
+app.use(express.json())
+ 
 app.get('/hello', (req,res)=>{
   res.send('Task Manager App')
 })
 
+app.use('/api/v1/tasks',tasks)
+
 app.listen(port,()=>{
   console.log(`SErver is listening on port ${port}`);
-})
+}) 
---
+++ 03-task-manager/starter/db/connect.js
@@ -0,0 +1,2 @@
+const connectionString = 
+'mongodb+srv://<username>:<password>@nodeexpressprojects.kv7mnq4.mongodb.net/?retryWrites=true&w=majority'
