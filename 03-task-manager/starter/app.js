6dc7ed12ba7e5676599fd9c4571c9c0c2091055e
Vitor Dilucca <vitordilucca@hotmail.com>
2023-04-08 10:20:08 -0300

    finalizando errorhandler e PROJETO FINALIZADO

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -4,7 +4,8 @@ const tasks=require('./routes/tasks')
 const connectDB=require('./db/connect')
 require('dotenv').config()
 const notFound = require('./middleware/not-found')
-const port = 3000
+const errorHandlerMiddleware = require('./middleware/error-handler')
+const port = process.env.PORT || 3000
 
 //middleware
 app.use(express.static('./public'))
@@ -13,6 +14,7 @@ app.use(express.json())
 //routes
 app.use('/api/v1/tasks',tasks)
 app.use(notFound)
+app.use(errorHandlerMiddleware)
 
 const start = async ()=>{
   try {
--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -1,5 +1,6 @@
 const Task = require('../models/task.js')
 const asyncWrapper = require('../middleware/async.js')
+const {createCustomError} = require('../errors/custom-error.js')
 //para testar todas estas funções voce vai ir no postman e vai mandar uma request de acordo com a funçao q vc quiser testar ex:getAllTasks=request GET, getTask=request GET, e vc vai ter q inserir um ID do item q vc quiser recuperar especifico no url
 const getAllTasks = asyncWrapper(async (req, res) => {
   const tasks = await Task.find({})
@@ -14,11 +15,11 @@ const createTask = asyncWrapper(async (req, res) => {
 })
 
 //para usar vai no postman e no endereço {{URL}}/tasks/{id} manda um GET req
-const getTask = asyncWrapper(async (req, res) => {
+const getTask = asyncWrapper(async (req, res,next) => {
   const { id: taskID } = req.params
   const task = await Task.findOne({ _id: taskID })
   if (!task) {
-    return res.status(404).json({ msg: `No task with id: ${taskID}` })
+    return next(createCustomError(`No task with id: ${taskID}`,404))
   }
   res.status(200).json({ task })
 })
@@ -28,7 +29,7 @@ const deleteTask = asyncWrapper(async (req, res) => {
   const { id: taskID } = req.params
   const task = await Task.findOneAndDelete({ _id: taskID })
   if (!task) {
-    return res.status(404).json({ msg: `No task with id: ${taskID}` })
+    return next(createCustomError(`No task with id: ${taskID}`,404))
   }
   // res.status(200).send()
   // res.status(200).json({task:null, status:'success'})
@@ -42,7 +43,7 @@ const updateTask = asyncWrapper(async (req, res) => {
     runValidators: true
   })
   if (!task) {
-    return res.status(404).json({ msg: `No task with id: ${taskID}` })
+    return next(createCustomError(`No task with id: ${taskID}`,404))
   }
   res.status(200).json({ task })
 })
