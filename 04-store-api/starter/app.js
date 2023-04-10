0adebdcfa016a48de46521597b913be5ed80407a
Vitor Dilucca <vitordilucca@hotmail.com>
2023-04-10 07:07:59 -0300

    arrumei bug q eu tinha colocado o .env na pasta errada imposibilitando a conexao com o mongodb, esta funcionando aqui voce entra na pagina http://localhost:3000/api/v1/products e products/static e recebe a mensagem

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -5,6 +5,7 @@ const express = require('express')
 const app = express()
 
 const connectDB = require('./db/connect')
+const productsRouter = require('./routes/products')
 
 const notFoundMiddleware = require('./middleware/not-found')
 const errorHandler = require('./middleware/error-handler')
@@ -16,6 +17,8 @@ app.use(express.json())
 app.get('/', (req,res)=>{
   res.send('<h1>Store API</h1><a href="/api/v1/products"Products Route>Products route</a>')
 })
+
+app.use('/api/v1/products', productsRouter)
 
 //products route
 app.use(notFoundMiddleware)
