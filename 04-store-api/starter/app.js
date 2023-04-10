59d278d9a30e9700bf1eb47fc6aa4ae05be2f477
Vitor Dilucca <vitordilucca@hotmail.com>
2023-04-10 06:43:54 -0300

    adicionado projeto 4 store api e feito algumas alterações iniciais

---
+++ 04-store-api/starter/app.js
@@ -0,0 +1,36 @@
+require('dotenv').config()
+//async errors
+
+const express = require('express')
+const app = express()
+
+const connectDB = require('./db/connect')
+
+const notFoundMiddleware = require('./middleware/not-found')
+const errorHandler = require('./middleware/error-handler')
+
+//middleware
+app.use(express.json())
+
+//routes
+app.get('/', (req,res)=>{
+  res.send('<h1>Store API</h1><a href="/api/v1/products"Products Route>Products route</a>')
+})
+
+//products route
+app.use(notFoundMiddleware)
+app.use(errorHandler)
+
+const port = process.env.PORT || 3000
+
+async function start(){
+  try {
+    //connectDB
+    await connectDB(process.env.MONGO_URI)
+    app.listen(port, console.log(`Server is listening port ${port}`))
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+start()
