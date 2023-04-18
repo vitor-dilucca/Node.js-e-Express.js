965011995f27348850ee3469211271d0e191fb90
Vitor Dilucca <vitordilucca@hotmail.com>
2023-04-18 16:27:12 -0300

    adicionado feature sort à API ex:http://localhost:3000/api/v1/products?sort=-name,price

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,16 +1,17 @@
 const Product = require('../models/product')
 
+// 4:20:00~ problema com o sort
 const getAllProductsStatic = async (req, res) => {
-  const search = 'ab'
-  const products = await Product.find({
-    name: { $regex: search, $options: 'i' },
-  })
+  const products = await Product.find({}).sort('-name price')
   // throw new Error('testing async errors')
-  res.status(200).json({ products, nbHits: products.length })
+  res.status(200).json({
+    nbHits: products.length,
+    products
+  })
 }
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name } = req.query
+  const { featured, company, name, sort } = req.query
   const queryObject = {}
 
   if (featured) {
@@ -22,9 +23,19 @@ const getAllProducts = async (req, res) => {
   if (name) {
     queryObject.name = { $regex: name, $options: 'i' }
   }
-  console.log(queryObject)
-  const products = await Product.find(queryObject)
-  res.status(200).json({ products, nbHist: products.length })
+  // console.log(queryObject)
+  let result = Product.find(queryObject)
+  if (sort) {
+    const sortList = sort.split(',').join(' ')
+    result = result.sort(sortList)
+  }else{
+    result = result.sort('createAt')
+  }
+  const products = await result
+  res.status(200).json({
+    nbHits: products.length,
+    products
+  })
 }
 
 
