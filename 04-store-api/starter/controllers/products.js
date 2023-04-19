6efa58cede7b2fbe1b4d97c3c9cee2dab7d2f015
Vitor Dilucca <vitordilucca@hotmail.com>
2023-04-19 12:35:24 -0300

    numeric filters, ex: 'http://localhost:3000/api/v1/products?numericFilters=price>40,rating>=4' vai te retornar no queryObject '{ price: { '': 40 }, rating: { '': 4 } }' que sao dados em um formato que voce pode manipular acho, todo o tratamento de dados e manipulaçao de strings foi feita aqui nesse comit

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -14,7 +14,7 @@ const getAllProductsStatic = async (req, res) => {
 }
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort, fields } = req.query
+  const { featured, company, name, sort, fields, numericFilters } = req.query
   const queryObject = {}
 
   if (featured) {
@@ -48,7 +48,29 @@ const getAllProducts = async (req, res) => {
 
   result = result.skip(skip).limit(limit)
 
+  if (numericFilters) {
+    const operatorMap = {
+      '>': '$gt',
+      '>=': '$gte',
+      '=': '$eq',
+      '<': '$lt',
+      '<': '$lte',
+    }
+    const regEx = /\b(<|>|>=|=|<|<=)\b/g
+    let filters = numericFilters.replace(
+      regEx,
+      (match) => `-${operatorMap[match]}-`
+    )
+    const options = ['price', 'rating']
+    filters = filters.split(',').forEach((item) => {
+      const [field, operator, value] = item.split('-')
+      if (options.includes(field)) {
+        queryObject[field] = {[operator]:Number(value)}
+      }
+    })
+  }
 
+  console.log(queryObject)
   const products = await result
   res.status(200).json({
     nbHits: products.length,
