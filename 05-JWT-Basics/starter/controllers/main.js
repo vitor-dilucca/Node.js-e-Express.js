6844bfaf7030b95dcaba672e2c05f4ebdb800b91
Vitor Dilucca <vitordilucca@hotmail.com>
2023-04-28 20:53:00 -0300

    alterado o error handling e implementado o uso da lib StatusCodes

--- 05-JWT-Basics/starter/controllers/main.js
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -11,34 +11,25 @@ const login = async (req, res) => {
   //joi
   //check in the controller
   if (!username || !password) {
-    throw new CustomAPIError('Please provide email and password',400)
+    throw new CustomAPIError('Please provide email and password', 400)
   }
   //demo, normaly provided bt DB
   const id = new Date().getDate()
   //try to keep payload small, better exp for user, (faster)
-  const token = jwt.sign({id, username},process.env.JWT_SECRET,{expiresIn:'30d'})
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, { expiresIn: '30d' })
 
-  res.status(200).json({msg:'user created',token})
+  res.status(200).json({ msg: 'user created', token })
 }
 
 const dashboard = async (req, res) => {
-  const authHeader = req.headers.authorization
+  console.log(req.user)
 
-  if(!authHeader || !authHeader.startsWith('Bearer ')){
-    throw new CustomAPIError('No token provided',401)
-  }
+  const luckyNumber = Math.floor(Math.random() * 100)
   
-  const token = authHeader.split(' ')[1]
-
-  try {
-    const decoded = jwt.verify(token,process.env.JWT_SECRET)
-    console.log(decoded);
-    const luckyNumber = Math.floor(Math.random() * 100)
-    res.status(200).json({ msg: `Hello, ${decoded.username}`, secret: `Here is your authorized data, your lucky number is ${luckyNumber}` })
-  } catch (error) {
-    throw new CustomAPIError('Not authorized to access this route',401)
-    
-  }
+  res.status(200).json({
+    msg: `Hello, ${req.user.username}`,
+    secret: `Here is your authorized data, your lucky number is ${luckyNumber}`
+  })
 
 }
 
---
+++ 05-JWT-Basics/starter/errors/unauthenticated.js
@@ -0,0 +1,10 @@
+const CustomAPIError = require("./custom-error")
+
+class UnauthenticatedError extends CustomAPIError {
+  constructor(message) {
+    super(message)
+    this.statusCode = StatusCodes.UNAUTHORIZED
+  }
+}
+
+module.exports = UnauthenticatedError
