1722d8faa394c06a33a132615735906379820e44
Vitor Dilucca <vitordilucca@hotmail.com>
2023-05-03 20:43:42 -0300

    projeto JOBS API aqui esta o projeto inicial com as pastas, e as ediçoes que fiz nos controllers, e no routes/auth

---
+++ 06-jobs-api/starter/controllers/jobs.js
@@ -0,0 +1,20 @@
+const getAllJobs = async(req,res)=>{
+  res.send('get all jobs')
+}
+const getJob = async(req,res)=>{
+  res.send('get job')
+}
+const createJob = async(req,res)=>{
+  res.send('get job')
+}
+const updateJob = async(req,res)=>{
+  res.send('get job')
+}
+const deleteJob = async(req,res)=>{
+  res.send('get job')
+}
+
+module.exports = {
+  getAllJobs,
+  getJob,createJob,updateJob,deleteJob
+}
