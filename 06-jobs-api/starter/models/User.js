87beabb117267029a4a5fd8b9f4f0293a5f32b1e
Vitor Dilucca <vitordilucca@hotmail.com>
2023-05-10 03:20:10 -0300

    alterações para encriptação de senha e outras coisas

--- 06-jobs-api/starter/models/User.js
+++ 06-jobs-api/starter/models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -14,7 +16,7 @@ const UserSchema = new mongoose.Schema({
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
       'Please provide valid email'
     ],
-    unique:true,
+    unique: true,
   },
   password: {
     type: String,
@@ -23,4 +25,19 @@ const UserSchema = new mongoose.Schema({
   },
 })
 
-module.exports = mongoose.model('User',UserSchema)
+UserSchema.pre('save', async function () {
+  const salt = await bcrypt.genSalt(10)
+  this.password = await bcrypt.hash(this.password, salt)
+})
+
+UserSchema.methods.createJWT = function () {
+  return jwt.sign(
+    { userId: this._id, name: this.name },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: process.env.JWT_LIFETIME
+    }
+  )
+}
+
+module.exports = mongoose.model('User', UserSchema)
