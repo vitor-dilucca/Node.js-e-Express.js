440d7786db35444c66c404cfb3dd1c49f00ee2a7
Vitor Dilucca <vitordilucca@hotmail.com>
2023-05-10 22:38:51 -0300

    controller jobs contendo getalljobs, getjob, updateJob e deleteJob. e começando a fazer o errorHandler para lidar com erro de email duplicado ao registrar

--- 06-jobs-api/starter/controllers/jobs.js
+++ 06-jobs-api/starter/controllers/jobs.js
@@ -1,20 +1,71 @@
-const getAllJobs = async(req,res)=>{
-  res.send('get all jobs')
+const Job = require('../models/Job')
+const { StatusCodes } = require('http-status-codes')
+const { BadRequestError, NotFoundError } = require('../errors')
+
+const getAllJobs = async (req, res) => {
+  const jobs = await Job.find({ createdBy: req.user.userId }).sort('createdAt')
+  res.status(StatusCodes.OK).json({ count: jobs.length, jobs })
 }
-const getJob = async(req,res)=>{
-  res.send('get job')
+const getJob = async (req, res) => {
+  const {
+    user: { userId },
+    params: { id: jobId }
+  } = req
+
+  const job = await Job.findOne({
+    _id: jobId,
+    createdBy: userId
+  })
+  if (!job) {
+    throw new NotFoundError(`No job with id ${jobId}`)
+  }
+  res.status(StatusCodes.OK).json({ job })
 }
-const createJob = async(req,res)=>{
-  res.send(req.user)
+const createJob = async (req, res) => {
+  req.body.createdBy = req.user.userId
+  const job = await Job.create(req.body)
+  res.status(StatusCodes.CREATED).json({ job })
 }
-const updateJob = async(req,res)=>{
-  res.send('update job')
+
+const updateJob = async (req, res) => {
+  const {
+    body: { company, position },
+    user: { userId },
+    params: { id: jobId }
+  } = req
+
+  if (company === '' || position === '') {
+    throw new BadRequestError('Company or Position fields cannot be empty')
+  }
+  const job = await Job.findByIdAndUpdate(
+    { _id: jobId, createdBy: userId },
+    req.body,
+    { new: true, runValidators: true }
+  )
+  if(!job){
+    throw new NotFoundError(`No job with id ${jobId}`)
+  }
+  res.status(StatusCodes.OK).json({job})
 }
-const deleteJob = async(req,res)=>{
-  res.send('delete job')
+
+
+const deleteJob = async (req, res) => {
+  const{
+    user:{userId},
+    params:{id:jobId},
+  } =req
+
+  const job = await Job.findByIdAndRemove({
+    _id:jobId,
+    createdBy:userId
+  })
+  if(!job){
+    throw new NotFoundError(`No job with id ${jobId}`)
+  }
+  res.status(StatusCodes.OK).send()
 }
 
 module.exports = {
   getAllJobs,
-  getJob,createJob,updateJob,deleteJob
+  getJob, createJob, updateJob, deleteJob
 }
