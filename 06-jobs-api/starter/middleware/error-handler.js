6f567549ab7de951624f61b351b4c917c73a0aea
Vitor Dilucca <vitordilucca@hotmail.com>
2023-05-14 04:46:55 -0300

    provendo mensagem de erro amigavel ao user caso faltar login e senha na hora de fazer login

--- 06-jobs-api/starter/middleware/error-handler.js
+++ 06-jobs-api/starter/middleware/error-handler.js
@@ -1,4 +1,3 @@
-const { CustomAPIError } = require('../errors')
 const { StatusCodes } = require('http-status-codes')
 const errorHandlerMiddleware = (err, req, res, next) => {
 
@@ -8,15 +7,21 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     msg:err.message || 'Something went wrong try again later'
   }
 
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message })
+  // if (err instanceof CustomAPIError) {
+  //   return res.status(err.statusCode).json({ msg: err.message })
+  // }
+
+  if(err.name ==='ValidationError'){
+    console.log(Object.values(err.errors))
+    customError.msg=Object.values(err.errors).map((item)=>item.message).join(',')
+    customError.statusCode=400
   }
   if(err.code && err.code === 11000){
     customError.msg = `Duplicate value entered for ${Object.keys(err.keyValue)}field, please choose another value`
     customError.statusCode = 400
   }
-  return res.status(customError.statusCode).json({ msg:customError.msg })
   // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
+  return res.status(customError.statusCode).json({ msg:customError.msg })
 
 }
 
