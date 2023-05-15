4957823de1db9d90d0f43f641154db4683c30373
Vitor Dilucca <vitordilucca@hotmail.com>
2023-05-15 05:17:52 -0300

    adicionando documentação Swagger para testar a API na web

--- 06-jobs-api/starter/app.js
+++ 06-jobs-api/starter/app.js
@@ -7,6 +7,11 @@ const cors = require('cors')
 const xss = require('xss-clean')
 const rateLimiter = require('express-rate-limit')
 
+//Swagger
+const swaggerUI = require('swagger-ui-express')
+const YAML = require('yamljs')
+const swaggerDocument = YAML.load('./swagger.yaml')
+
 const express = require('express');
 const app = express();
 
@@ -36,8 +41,9 @@ app.use(cors())
 app.use(xss())
 
 app.get('/',(req,res)=>{
-  res.send('jobs api')
+  res.send('<h1>jobs API</H1> <a href="/api-docs">Documentação</a>')
 })
+app.use('/api-docs',swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 
   // routes
 app.use('/api/v1/auth',authRouter)
